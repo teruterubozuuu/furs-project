5798687b1fc3f198e14de9df515c5fb0469aca83
Joylynne Esportuno <degozarunyan@gmail.com>
2025-10-25 19:11:15 +0800

    KAN-41 feature/admin-dashboard feat: initialize Firebase Admin SDK and admin routes

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { signOut } from 'firebase/auth'
+import { auth} from '../../firebase/config'
+import { useNavigate } from 'react-router-dom';
 
 export default function () {
+
+  const navigate = useNavigate();
+
+  const logOut = async() => {
+    try{
+      await signOut(auth);
+      navigate("/login")
+    } catch (error){
+      console.log(error);
+    }
+  };
+
   return (
     <div>
         <h1>Dashboard</h1>
+        <button onClick={logOut}>Logout</button>
     </div>
   )
 }
--- src/routes/RedirectIfAuthenticated.jsx
+++ src/routes/RedirectIfAuthenticated.jsx
@@ -1,12 +1,46 @@
-import { useAuth } from "../context/AuthContext";
 import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase/config";
 
 export default function RedirectIfAuthenticated({ children }) {
   const { user } = useAuth();
   const location = useLocation();
+  const [userRole, setUserRole] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  if (user && (location.pathname === "/login" || location.pathname === "/register")) {
-    return <Navigate to="/home" replace />;
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      if (!user) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        if (userDoc.exists()) {
+          setUserRole(userDoc.data().userType);
+        }
+      } catch (err) {
+        console.error("Error fetching user role:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserRole();
+  }, [user]);
+
+  if (loading) return null; // or a loader component
+
+  if (user && (location.pathname === "/login" || location.pathname === "/signup")) {
+    if (userRole === "Admin") {
+      return <Navigate to="/admin/dashboard" replace />;
+    } else {
+      return <Navigate to="/home" replace />;
+    }
   }
+
   return children;
 }
