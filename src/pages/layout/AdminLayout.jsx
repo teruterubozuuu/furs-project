ca499cadf82d146d1e1b547194c46cb486c7f280
Joylynne Esportuno <degozarunyan@gmail.com>
2025-10-27 13:44:34 +0800

    KAN-41 feature/admin-dashboard style(navbar, layout): add active links styling to navigation links in the navbar and fix padding in admin layout

--- src/pages/layout/AdminLayout.jsx
+++ src/pages/layout/AdminLayout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../admin/components/Navbar"
 export default function AdminLayout({children}) {
   return (
     <div className='flex lg:flex-row flex-col bg-[#0E1911] lg:h-screen w-full'>
-      <aside className='py-5 px-8 w-full lg:max-w-[180px] '>
+      <aside className='py-5 w-full lg:max-w-[180px] '>
         <Navbar/>
       </aside>
         <main className='bg-white w-full rounded-tl-[30px] rounded-tr-[30px] lg:rounded-tr-none lg:rounded-bl-[30px] py-5 px-10'>
